6220621115e611cc04cbdb2b66b898a31a8f0d0d
Hugo Silva <75398814+HugoF10@users.noreply.github.com>
2021-10-31 17:03:17 -0300

    Inserindo o exercicio 1 - Realizado com métodos na própria classe do programa!

---
+++ Exercicio - 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o valor de raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double calc = Area(raio);
+
+            Console.WriteLine("Area: " + calc);
+        }
+
+        static double Area(double r)
+        {
+            return Math.PI * (r * r);
+        }
+
+
+    }
+}
+
